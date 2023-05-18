63ebfa9999a4e8b85b2e93c75ef1a43cdf8a2067
Kevin Mondesir <kevinmondesir95@gmail.com>
2023-05-18 11:20:11 +0200

    Add Parameter, its helper function and tests

---
+++ RepositoryGeneratorPublicModel/Sources/RepositoryGeneratorPublicModel/Models/Function/Parameter/Parameter.swift
@@ -0,0 +1,82 @@
+//
+//  Parameter.swift
+//  
+//
+//  Created by Kévin Mondésir on 17/05/2023.
+//
+
+import Foundation
+
+public struct Parameter {
+    public let label: String?
+    public let name: String
+    public let originalName: String
+    public let type: String
+    
+    public init(label: String?, name: String, originalName: String? = nil, type: String) {
+        self.label = label
+        self.name = name
+        self.originalName = originalName ?? name
+        self.type = type
+    }
+    
+    public var signatureDescription: String {
+        guard let label else {
+            return "\(self.name): \(self.type)"
+        }
+        return "\(label) \(self.name): \(self.type)"
+    }
+    
+    public var callDescription: String {
+        guard let label else {
+            return "\(self.name): \(self.name)"
+        }
+        guard label == "_" else {
+            return "\(label): \(self.name)"
+        }
+        return self.name
+    }
+    
+    public var customInputCallDescription: String {
+        guard let label else {
+            return "\(self.name): <#T## enter value ###".appending(">")
+        }
+        guard label == "_" else {
+            return "\(label): <#T## enter value ###".appending(">")
+        }
+        return "<#T## enter value ###".appending(">")
+    }
+}
+
+extension Array<Parameter> {
+    public func signatureDescription() -> String {
+        return self
+            .map { $0.signatureDescription }
+            .joined(separator: ", ")
+    }
+
+    public func callDescription() -> String {
+        return self
+            .map { $0.callDescription }
+            .joined(separator: ", ")
+    }
+
+    public func testSignatureDescription(_ spacingLevel: UInt = 2) -> String {
+        let spacing = (0..<spacingLevel)
+            .map { _ in "\t" }
+            .joined()
+        return self
+            .map { spacing + $0.signatureDescription }
+            .joined(separator: ",\n")
+    }
+    
+    public func customInputCallDescription(_ spacingLevel: UInt = 3) -> String {
+        guard !self.isEmpty else { return "" }
+        let spacing = (0..<spacingLevel)
+            .map { _ in "\t" }
+            .joined()
+        return self
+            .map { spacing + $0.customInputCallDescription }
+            .joined(separator: ",\n")
+    }
+}
