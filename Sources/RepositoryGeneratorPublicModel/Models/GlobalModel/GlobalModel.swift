530751fffa499677312c7bce7068fb89b1d94da0
Kevin Mondesir <kevinmondesir95@gmail.com>
2023-08-11 21:06:06 +0200

    Add additionalInformation property to the GlobalModel

--- Sources/RepositoryGeneratorPublicModel/Models/GlobalModel/GlobalModel.swift
+++ Sources/RepositoryGeneratorPublicModel/Models/GlobalModel/GlobalModel.swift
@@ -11,10 +11,12 @@ extension RepositoryGeneratorPublicModel {
     public struct GlobalModel: Codable {
         public let operations: [Operation]
         public let schemaObjects: [SchemaObject]
+        public let additionalInformation: [String: String]
 
-        public init(operations: [Operation], schemaObjects: [SchemaObject]) {
+        public init(operations: [Operation], schemaObjects: [SchemaObject], additionalInformation: [String: String]) {
             self.operations = operations
             self.schemaObjects = schemaObjects
+            self.additionalInformation = additionalInformation
         }
     }
 }
