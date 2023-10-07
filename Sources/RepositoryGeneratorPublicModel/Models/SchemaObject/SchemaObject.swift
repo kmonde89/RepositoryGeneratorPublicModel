9803a5432fe30667d9d6b4ae6838526d303bb2c1
Kevin Mondesir <kevinmondesir95@gmail.com>
2023-10-07 16:30:27 +0200

    Add documentation for SchemaObject

--- Sources/RepositoryGeneratorPublicModel/Models/SchemaObject/SchemaObject.swift
+++ Sources/RepositoryGeneratorPublicModel/Models/SchemaObject/SchemaObject.swift
@@ -7,17 +7,37 @@
 
 import Foundation
 
+
+
 extension RepositoryGeneratorPublicModel {
+    /**
+     Representation of an open api schema object.
+
+     Schema Object allows the definition of input and output data types.
+     See open api [documentation](https://swagger.io/specification/#schema-object).
+     */
     public class SchemaObject: Codable {
-        /// Object name  from the openAPI document
+        /// Object name  from the openAPI document. Only available for referenced schemaObject.
+        ///
+        ///Yaml representation of a schema object:
+        /// ```
+        ///components:
+        ///  schemas:
+        ///    Pepper:
+        ///      type: object
+        ///      properties:
+        ///        name:
+        ///          type: string
+        /// ```
+        /// For this example the ``name`` will be set with the value **Pepper**
         public let name: String?
         /// Object custom name (alias set using RepoScribe)
         public let customName: String?
         /// Object reference  from the openAPI document
         public let ownReference: String?
-        /// Object type  from the openAPI document
+        /// The ``type`` property following [json-schema-validation specification](https://datatracker.ietf.org/doc/html/draft-bhutton-json-schema-validation-00#section-6.1.1) followed by openapi can either be *"null"*, *"boolean"*, *"object"*, *"array"*, *"number"*, *"integer"* or *"string"*.
         public let type: String?
-        /// Object custom type (alias set using RepoScribe)
+        /// The ``customType`` is a custom made variation of the ``type`` property that can be set  using RepoScribe application.
         public let customType: String?
         /// Object description from the openAPI document
         public let description: String?
@@ -27,12 +47,60 @@ extension RepositoryGeneratorPublicModel {
         public let enumeration: [String]?
         /// Object propertyKey from the openAPI document
         public let propertyKey: String?
+        /// Object custom propertyKey (alias set using RepoScribe)
         public let customPropertyKey: String?
+
+        ///Childrens will are representation of properties within a schema object.
+        ///
+        ///Yaml representation of a schema object:
+        /// ```
+        /// type: object
+        /// properties:
+        ///   id:
+        ///     type: integer
+        ///     format: int64
+        ///   name:
+        ///     type: string
+        /// ```
+        /// In this example ``childrens`` will contains two ``SchemaObject`` one for the id and another for the name
         public let childrens: [SchemaObject]
+        /// The required property is the list of property within a schema object that are required.
+        ///
+        /// Yaml represetation of a schema object
+        /// ```
+        /// type: object
+        /// properties:
+        ///   id:
+        ///     type: integer
+        ///     format: int64
+        ///   name:
+        ///     type: string
+        /// required:
+        /// - name
+        /// ```
+        /// In this example the name property shouldn't be absent when sending or receiving this schemaObject from the api call.
+        ///
         public let required: [String]?
+        /// The reference property is available for schemaObject possesing a reference within the openapi specification.
+        ///
+        ///Yaml representation of a schema object:
+        /// ```
+        ///components:
+        ///  schemas:
+        ///    Pepper:
+        ///      type: object
+        ///      properties:
+        ///        name:
+        ///          type: string
+        /// ```
+        /// For this example the ``reference`` property will be set with the value **"#/components/schemas/Pepper"**.
+        ///
         public let reference: String?
+        /// The ``minimum`` property represent an inclusive lower limit for a numeric instance.
         public let minimum: Double?
+        /// The ``maximum`` property represent an inclusive lower limit for a numeric instance.
         public let maximum: Double?
+        /// The ``items`` property is only available for schemaObject of ``type``  **array**
         public let items: SchemaObject?
 
         public init(name: String? = nil, customName: String? = nil, ownReference: String? = nil, type: String? = nil, customType: String? = nil, description: String? = nil, format: String? = nil, enumeration: [String]? = nil, propertyKey: String? = nil, customPropertyKey: String? = nil, childrens: [SchemaObject] = [], required: [String]? = nil, reference: String? = nil, minimum: Double? = nil, maximum: Double? = nil, items: SchemaObject? = nil) {
@@ -54,6 +122,7 @@ extension RepositoryGeneratorPublicModel {
             self.items = items
         }
 
+        /// Computed variable displaying the object type when it is possible and displaying a placeholder when it is not possible.
         public var typeDescription: String {
             self.getType("Enter type") { _ in return nil}
         }
