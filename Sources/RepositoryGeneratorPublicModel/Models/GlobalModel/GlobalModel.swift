4c74a0df1441072efec759430ec4f3ad589eda1f
Kevin Mondesir <kevinmondesir95@gmail.com>
2025-02-02 20:50:30 +0100

    Add repository name to GlobalModel

--- Sources/RepositoryGeneratorPublicModel/Models/GlobalModel/GlobalModel.swift
+++ Sources/RepositoryGeneratorPublicModel/Models/GlobalModel/GlobalModel.swift
@@ -9,11 +9,13 @@ import Foundation
 
 extension RepositoryGeneratorPublicModel {
     public struct GlobalModel: Codable {
+        public let repositoryName: String
         public let operations: [Operation]
         public let schemaObjects: [SchemaObject]
         public let additionalInformation: [String: String]
 
-        public init(operations: [Operation], schemaObjects: [SchemaObject], additionalInformation: [String: String]) {
+        public init(repositoryName: String, operations: [Operation], schemaObjects: [SchemaObject], additionalInformation: [String: String]) {
+            self.repositoryName = repositoryName
             self.operations = operations
             self.schemaObjects = schemaObjects
             self.additionalInformation = additionalInformation
